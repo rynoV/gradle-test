cff0b1189788a0da25bd64576b799bae339941dc
Calum Sieppert <sieppertcalum@gmail.com>
2020-02-04 09:31:26 -0700

    Testing

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,5 @@
+public class ExampleClass {
+  public static boolean exampleMethod() {
+    return true;
+  }
+}
---
+++ src/test/java/ExampleClassTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleClassTest {
+
+  @Test
+  public void exampleMethod() {
+    assertTrue("Should return true.", ExampleClass.exampleMethod());
+    //    assertFalse("Should fail", ExampleClass.exampleMethod());
+  }
+}
